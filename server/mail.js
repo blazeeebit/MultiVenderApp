36f53b793338eca7eb3381d1531f7ca59b0d74a3
Blazeee Bit <59376763+blazeeebit@users.noreply.github.com>
2022-02-16 12:41:11 +0500

    Update mail.js

--- server/mail.js
+++ server/mail.js
@@ -1,14 +1,14 @@
 // const mailer = (email) => {
 const mailgun = require('mailgun-js');
-const DOMAIN = 'https://api.mailgun.net/v3/sandbox9029a9557b7248f6b446417a411b3308.mailgun.org';
+const DOMAIN = '';
 const mg = mailgun({
-	apiKey: '2799e8724157c274127ba3e298a2ee5e-7dcc6512-40e1803f',
+	apiKey: '',
 	domain: DOMAIN,
 	host: 'api.eu.mailgun.net'
 });
 const data = {
-	from: 'Leatwear <blazeeebit@gmail.com>',
-	to: 'syed.nauroz.ali.syed@gmail.com',
+	from: 'Leatwear',
+	to: '',
 	subject: 'Welcome to Leatwear',
 	text: 'Your Registration Was A Success!!'
 };
